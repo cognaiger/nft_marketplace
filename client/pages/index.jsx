11bc84b907da3a8e6cec7b4a5f01fc8ac5aa844c
cognaiger <21020369@vnu.edu.vn>
2023-11-04 21:25:03 +0700

    add servie, hero section

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Style from "../styles/index.module.css";
+import { HeroSection, Service } from "../components/componentsindex";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+    </div>
+  )
 };
 
 export default Home;
