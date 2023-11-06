6252a195b7f4da359aeb42f54be757a536614867
cognaiger <21020369@vnu.edu.vn>
2023-11-06 13:13:58 +0700

    add components

--- client/components/componentsindex.js
+++ client/components/componentsindex.js
@@ -3,5 +3,9 @@ import Footer from "./Footer/Footer";
 import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
+import BigNFTSilder from "./BigNFTSlider/BigNFTSlider";
+import Subscribe from "./Subcribe/Subcribe";
+import Title from "./Title/Title";
+import Category from "./Category/Category";
 
-export { Button, Footer, NavBar, HeroSection, Service };
+export { Button, Footer, NavBar, HeroSection, Service, BigNFTSilder, Subscribe, Title, Category };
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Style from "../styles/index.module.css";
-import { HeroSection, Service } from "../components/componentsindex";
+import { BigNFTSilder, Category, HeroSection, Service, Subscribe, Title } from "../components/componentsindex";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
+      <BigNFTSilder />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories." 
+      />
+      <Category />
+      <Subscribe />
     </div>
   )
 };
