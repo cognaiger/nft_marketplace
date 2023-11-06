749c0690c1cdbb0b3d9e58a5e51fc34d605dbbe9
cognaiger <21020369@vnu.edu.vn>
2023-11-06 16:38:38 +0700

    add collection page

---
+++ client/collectionPage/Banner/Banner.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/legacy/image";
+import Style from "./Banner.module.css";
+
+const Banner = ({ bannerImage }) => {
+    return (
+        <div className={Style.banner}>
+            <div className={Style.banner_img}>
+                <Image
+                    src={bannerImage}
+                    objectFit="cover"
+                    alt="background"
+                    width={2000}
+                    height={300}
+                />
+            </div>
+
+            <div className={Style.banner_img_mobile}>
+                <Image
+                    src={bannerImage}
+                    objectFit="cover"
+                    alt="background"
+                    width={1600}
+                    height={900}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
